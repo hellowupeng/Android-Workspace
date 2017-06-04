d71f39397d4c9dd6f9f5882b8dedbfce7f8c68ce
hellowupeng <hellowupeng@gmail.com>
2017-06-04 09:02:30 +0800

    add widget demo

--- UIWidgetTest/app/src/main/java/com/gaiaworks/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/gaiaworks/uiwidgettest/MainActivity.java
@@ -1,15 +1,24 @@
 package com.gaiaworks.uiwidgettest;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText mEditText;
+
+    private ImageView mImageView;
+
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +28,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
         mEditText = (EditText) findViewById(R.id.edit_text);
+        mImageView = (ImageView) findViewById(R.id.image_view);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button:
-                String inputText = mEditText.getText().toString();
-                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("This is ProgressDialog");
+                progressDialog.setMessage("Loading...");
+                progressDialog.setCancelable(true);
+                progressDialog.show();
                 break;
             default:
                 break;
